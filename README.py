ffda1eb51d7297960acbd75b91fc7cd5a788f98c
Yanisbt <yanis.biet@gmail.com>
2025-01-30 11:54:44 +0100

    Update README.py

--- README.py
+++ README.py
@@ -2,8 +2,8 @@ donnees={"Order ID":"","Product":"","Quantity Ordered":"","Price Each":"","Order
 def lire(donnees,fichier):
     with open(fichier,"w") as f:
         for keys in donnees.keys():
-            f.write(keys,"\n")
+            f.write(f"{keys}\n")
     affichage = print(fichier)
     return affichage
 print(lire(donnees,"PME.csv"))
-def organiser(donnees,fichier):
+        
