e0fa26cc8d0436c99874f8291c0ab2d6f311bc83
HarryBhn <harry.beghin123@gmail.com>
2025-01-30 12:21:32 +0100

    Update README.py

--- README.py
+++ README.py
@@ -8,7 +8,7 @@ def charger(donnees,fichier):
 print(charger(donnees,"PME.csv"))
         
 
-date=input("Entrez une date en str")
-produit= input("Entrez un produit en str")
+date=input("Entrez une date en toute lettre")
+produit= input("Entrez un produit en toute lettre")
 vente_prix=tuple.int("Entrez un minimum et un maximum pour le prix")
 vente_quantite=tuple.int("Entrez un minimum et un maximum pour la quantité")
